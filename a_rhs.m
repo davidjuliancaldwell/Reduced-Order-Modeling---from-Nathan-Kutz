d64d998ef95f29a4c8b26e97edc16cbfafe28c3b
David Caldwell <djcald@cs.washington.edu>
2016-10-18 17:44:22 -0700

    changes after class 10-18-2016

--- a_rhs.m
+++ a_rhs.m
@@ -1,3 +1,3 @@
 function rhs=a_rhs(t,a,dummy,phi,Lr)
 
-rhs= Lr*a + i*phi'*( (abs(phi*a).^2).*(phi*a) );
+rhs= Lr*a + i*phi'*( (abs(phi*a).^2).*(phi*a) ); % here you have to do the complex conjugate transpose - dont put dot!! if you do, messes it all up 
--- rom_nls.m
+++ rom_nls.m
@@ -5,7 +5,7 @@ x2=linspace(-L/2,L/2,n+1); x=x2(1:n);
 k=(2*pi/L)*[0:n/2-1 -n/2:-1].';
 t=linspace(0,2*pi,51);
 
-u=2*sech(x);
+u=2*sech(x); % initial conditions 
 ut=fft(u);
 
 [t,utsol]=ode45('nls_rhs',t,ut,[],k);
@@ -27,14 +27,17 @@ plot(diag(s)/sum(diag(s)),'ko','Linewidth',[2])
 figure(3)
 plot(real(u(:,1:3)),'Linewidth',[2])
 
+figure(5)
+plot(real(v(:,1:3)),'Linewidth',[2])
+
 r=4;  % rank of projection
 phi=u(:,1:r);  % Phi_r POD modes
 
 for j=1:r
-  phixx(:,j)=  -ifft((k.^2).*fft(phi(:,j))); % second derivatives
+  phixx(:,j)=  -ifft((k.^2).*fft(phi(:,j))); % second derivatives % this is the L Phi term % stack em up all up 
   a0(j)= 2*sech(x)*conj(phi(:,j));  % projection of initial conditions
 end
-Lr= (i/2)* phi'*phixx;  % Low-rank approximation of linear term
+Lr= (i/2)* phi'*phixx;  % Low-rank approximation of linear term % this is the 2x2 that you only have to do once, and then feed it in 
 
 [t,asol]=ode45('a_rhs',t,a0,[],phi,Lr);
 
@@ -45,7 +48,7 @@ for j=1:length(t)
        us(:,j)=us(:,j) + asol(j,jj)*phi(:,jj);  % r-rank reconstruction
     end
 end
-
+figure(4)
 surfl(x,t,abs(us.')); shading interp, colormap(hot);
 
 
